3bd70ffb4e55bec2118a90e842e393cc95f4dea4
alpiy <alfiahmad327@gmail.com>
2023-12-12 09:33:59 +0700

    Room Persistance Part 3

--- app/src/main/java/com/example/crudroom/MainActivity.kt
+++ app/src/main/java/com/example/crudroom/MainActivity.kt
@@ -2,10 +2,100 @@ package com.example.crudroom
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.content.Intent
+import android.util.Log
+import androidx.appcompat.app.AlertDialog
+import androidx.recyclerview.widget.LinearLayoutManager
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import com.example.crudroom.databinding.ActivityMainBinding
+import com.example.crudroom.room.Constant
+import com.example.crudroom.room.Note
+import com.example.crudroom.room.NoteDB
+
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    lateinit var noteAdapter: NoteAdapter
+
+    val db by lazy { NoteDB(this) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setupListener()
+        setupRecyclerView()
+    }
+
+    override fun onStart() {
+        super.onStart()
+        loadNote()
+    }
+
+    fun loadNote() {
+        CoroutineScope(Dispatchers.IO).launch {
+            val note = db.noteDao().getNote()
+            Log.d("MainActivity", "dbResponse: $note")
+            withContext(Dispatchers.Main) {
+                noteAdapter.setData(note)
+            }
+        }
+    }
+
+    private fun setupListener() {
+        binding.buttonCreate.setOnClickListener {
+            intentEdit(0, Constant.TYPE_CREATE)
+        }
+    }
+
+    fun intentEdit(noteId: Int, intentType: Int) {
+        startActivity(
+            Intent(applicationContext, EditActivity::class.java)
+                .putExtra("intent_id", noteId)
+                .putExtra("intent_type", intentType)
+        )
+    }
+
+    private fun setupRecyclerView() {
+        noteAdapter = NoteAdapter(arrayListOf(), object : NoteAdapter.OnAdapterListener{
+            override fun onRead(note: Note) {
+                intentEdit(note.id, Constant.TYPE_READ)
+            }
+
+            override fun onUpdate(note: Note) {
+                intentEdit(note.id, Constant.TYPE_UPDATE)
+            }
+
+            override fun onDelete(note: Note) {
+                deleteDialog(note)
+            }
+        })
+        binding.listNote.apply {
+            layoutManager = LinearLayoutManager(applicationContext)
+            adapter = noteAdapter
+        }
+    }
+
+    private fun deleteDialog(note: Note) {
+        val alertDialog = AlertDialog.Builder(this)
+        alertDialog.apply {
+            setTitle("Konfirmasi")
+            setMessage("Yakin Hapus ${note.title}?")
+            setNegativeButton("Batal") { dialogInterface, i ->
+                dialogInterface.dismiss()
+            }
+            setPositiveButton("Hapus") { dialogInterface, i ->
+                dialogInterface.dismiss()
+                CoroutineScope(Dispatchers.IO).launch {
+                    db.noteDao().deleteNote(note)
+                    loadNote()
+                }
+            }
+        }
+        alertDialog.show()
     }
 }
---
+++ app/src/main/java/com/example/crudroom/NoteAdapter.kt
@@ -0,0 +1,54 @@
+package com.example.crudroom
+
+import androidx.recyclerview.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import com.example.crudroom.databinding.AdapterMainBinding
+import com.example.crudroom.room.Note
+import java.util.EventListener
+
+class NoteAdapter (private var notes : ArrayList<Note>,
+    private val listener: OnAdapterListener) :
+    RecyclerView.Adapter<NoteAdapter.NoteViewHolder>(){
+
+        inner class  NoteViewHolder(val binding: AdapterMainBinding):
+                RecyclerView.ViewHolder(binding.root)
+
+        interface  OnAdapterListener{
+            fun onRead(note: Note)
+            fun onUpdate(note: Note)
+            fun onDelete(note: Note)
+        }
+
+        private lateinit var   binding: AdapterMainBinding
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NoteViewHolder {
+        val binding = AdapterMainBinding.inflate(LayoutInflater.from(parent.context),
+        parent,false)
+        return  NoteViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: NoteViewHolder, position: Int) {
+        with(holder) {
+            with(notes[position]){
+                binding.textTitle.text = title
+                binding.textTitle.setOnClickListener{
+                    listener.onRead(notes[position])
+                }
+                binding.iconEdit.setOnClickListener{
+                    listener.onUpdate(notes[position])
+                }
+                binding.iconDelete.setOnClickListener{
+                    listener.onDelete(notes[position])
+                }
+            }
+        }
+    }
+
+    override fun getItemCount() = notes.size
+    fun setData(newList: List<Note>){
+        notes.clear()
+        notes.addAll(newList)
+        notifyDataSetChanged()
+    }
+
+}
